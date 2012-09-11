d6c106556e887b1463cb78bd86af241670499190
Paul Ecoffet <ecoffet.paul@gmail.com>
2012-09-11 17:38:03 +0200

    URI parsing is now correctly
    working thanks to the android API

--- Nexyu/src/org/nexyu/nexyuAndroid/MainActivity.java
+++ Nexyu/src/org/nexyu/nexyuAndroid/MainActivity.java
@@ -8,6 +8,7 @@ import android.app.Activity;
 import android.content.ComponentName;
 import android.content.Intent;
 import android.content.ServiceConnection;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.IBinder;
 import android.os.Message;
@@ -101,32 +102,25 @@ public class MainActivity extends Activity implements View.OnClickListener
 		if (result != null)
 		{
 			String contents = result.getContents();
-			String urischeme = "nexyu";
-			// ipregex found on
-			// http://stackoverflow.com/questions/4011855/regexp-to-check-if-an-ip-is-valid
-			String ipregex = "\\b(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\b";
-			//portregex found on http://social.msdn.microsoft.com/Forums/sk/regexp/thread/bc7f5bc2-ac30-434b-9ae9-908d602ee6ea
-			String portregex = "(6553[0-5]|655[0-2]\\d|65[0-4]\\d{2}|6[0-4]\\d{3}|[1-5]\\d{4}|[1-9]\\d{0,3})";
-			String controlregex = "(\\d{5})";
-			if (contents != null
-					&& contents.matches(urischeme + "://" + ipregex + ":" + portregex + "\\?"
-							+ controlregex))
+			Uri path = Uri.parse(contents);
+			if (path.getScheme().equals("nexyu"))
 			{
-				String ip = contents.replaceAll(ipregex, "$1");
-				int port = Integer.parseInt(contents.replaceAll(portregex, "$1"));
-				int verificationCode = Integer.parseInt(contents.replaceAll(controlregex, "$1"));
-				sendConnectionMessage(ip, port, verificationCode);
+				String host = path.getHost();
+				int port = path.getPort();
+				String verificationCode = path.getQueryParameter("verif");
+				if(host != null && port != -1 && verificationCode != null)
+					sendConnectionMessage(host, port, verificationCode);
 			}
 		}
 	}
 
-	private void sendConnectionMessage(String ip, int port, int verificationCode)
+	private void sendConnectionMessage(String host, int port, String verificationCode)
 	{
 		Message connect = Message.obtain(null, NexyuService.MSG_CONNECT);
 		Bundle data = new Bundle();
-		data.putString("ip", ip);
+		data.putString("ip", host);
 		data.putInt("port", port);
-		data.putInt("verificationCode", verificationCode);
+		data.putString("verificationCode", verificationCode);
 		connect.setData(data);
 		try
 		{
