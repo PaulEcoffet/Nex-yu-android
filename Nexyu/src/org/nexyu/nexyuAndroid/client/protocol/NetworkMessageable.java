f30ddf34cac1f524501422b9b430284ab9039577
Paul Ecoffet <ecoffet.paul@gmail.com>
2012-09-24 22:12:07 +0200

    Addition of copyright preamble before each java source files

--- Nexyu/src/org/nexyu/nexyuAndroid/client/protocol/NetworkMessageable.java
+++ Nexyu/src/org/nexyu/nexyuAndroid/client/protocol/NetworkMessageable.java
@@ -1,5 +1,20 @@
-/**
- *
+/*
+ * Copyright 2012 Nex yu Android authors
+ * 
+ * This file is part of Nex yu Android.
+ * 
+ * Nex yu Android is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your option) any
+ * later version.
+ * 
+ * Nex yu Android is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ * 
+ * You should have received a copy of the GNU General Public License along with
+ * Nex yu Android. If not, see <http://www.gnu.org/licenses/>
  */
 package org.nexyu.nexyuAndroid.client.protocol;
 
@@ -7,7 +22,7 @@ package org.nexyu.nexyuAndroid.client.protocol;
  * An object implementing this interface is able to be converted into a
  * {@link NetworkMessage}, which is required by the Pipeline to be sent over the
  * network.
- *
+ * 
  * @author Paul Ecoffet
  */
 public interface NetworkMessageable
@@ -15,7 +30,7 @@ public interface NetworkMessageable
 	/**
 	 * Convert the object implementing the {@link NetworkMessageable} interface
 	 * into a {@link NetworkMessage} so as to be sent over the network.
-	 *
+	 * 
 	 * @return The {@link NetworkMessageable} object converted into a
 	 *         {@link NetworkMessage}
 	 */
--- Nexyu/src/org/nexyu/nexyuAndroid/utils/ArrayUtils.java
+++ Nexyu/src/org/nexyu/nexyuAndroid/utils/ArrayUtils.java
@@ -1,5 +1,20 @@
-/**
- *
+/*
+ * Copyright 2012 Nex yu Android authors
+ * 
+ * This file is part of Nex yu Android.
+ * 
+ * Nex yu Android is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your option) any
+ * later version.
+ * 
+ * Nex yu Android is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ * 
+ * You should have received a copy of the GNU General Public License along with
+ * Nex yu Android. If not, see <http://www.gnu.org/licenses/>
  */
 package org.nexyu.nexyuAndroid.utils;
 
@@ -9,21 +24,23 @@ import android.util.SparseArray;
 
 /**
  * @author Paul Ecoffet
- *
+ * 
  */
 public class ArrayUtils
 {
 
 	/**
 	 * Convert a sparseArray into an ArrayList
-	 * @param sparseArray The SparseArray to convert
+	 * 
+	 * @param sparseArray
+	 *            The SparseArray to convert
 	 * @return A ArrayList containing the elements of the SparseArray.
 	 */
 	public static <T> ArrayList<T> SparseArrayToArrayList(SparseArray<T> sparseArray)
 	{
 		ArrayList<T> result = new ArrayList<T>();
 		int size = sparseArray.size();
-		for(int i = 0; i < size; i++)
+		for (int i = 0; i < size; i++)
 		{
 			result.add(sparseArray.valueAt(i));
 		}
