0e70df38bbc3689b403578312565c454de124b02
Paul Ecoffet <ecoffet.paul@gmail.com>
2012-10-06 10:29:07 +0200

    Implementation of SherlockActionBar and some correction in the display

--- Nexyu/src/org/nexyu/nexyuAndroid/ui/MainActivity.java
+++ Nexyu/src/org/nexyu/nexyuAndroid/ui/MainActivity.java
@@ -23,7 +23,6 @@ import org.nexyu.nexyuAndroid.service.NexyuService;
 import org.nexyu.nexyuAndroid.utils.IntentIntegrator;
 import org.nexyu.nexyuAndroid.utils.IntentResult;
 
-import android.app.Activity;
 import android.content.ComponentName;
 import android.content.Intent;
 import android.content.ServiceConnection;
@@ -33,12 +32,13 @@ import android.os.IBinder;
 import android.os.Message;
 import android.os.Messenger;
 import android.os.RemoteException;
-import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
-public class MainActivity extends Activity implements View.OnClickListener
+import com.actionbarsherlock.app.SherlockActivity;
+
+public class MainActivity extends SherlockActivity implements View.OnClickListener
 {
 	private Messenger			mService	= null;
 	private boolean				mBound		= false;
@@ -69,13 +69,6 @@ public class MainActivity extends Activity implements View.OnClickListener
 		Button connectButton = (Button) findViewById(R.id.ConnectBut);
 		bindService(new Intent(this, NexyuService.class), mConnection, BIND_AUTO_CREATE);
 		connectButton.setOnClickListener(this);
-	}
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu)
-	{
-		getMenuInflater().inflate(R.menu.activity_main, menu);
-		return true;
 	}
 
 	@Override
